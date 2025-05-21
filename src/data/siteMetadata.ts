b91483429f5de226d4085a4573817da74e9a0dcd
dat <goodboy30112003@gmail.com>
2025-05-21 18:18:13 +0700

    fix metadata

--- src/data/siteMetadata.ts
+++ src/data/siteMetadata.ts
@@ -1,47 +1,12 @@
-
- const siteMetadata = {
+const siteMetadata = {
   title: "dat's profile",
   author: 'Tien Dat',
-  jobTitle:"Web Developer",
-  major:"Information Technology",
-  university:"economics, engineering and industry (uneti)",
-  passion:[
-    "Coding",
-    "develop web application",
-  ],
-  technicalSkills: [
-    "nextjs",
-    "typescript",
-    "tailwindcss"
-  ],
-  database:[
-    "mongodb",
-    "mysql",
-  ],
-  orm:[
-    "prisma",
-    "typeorm",
-  ],
-  siteUrl:"http://localhost:3000",
-  born:"Nam Định",
-  fullName: 'luu Tien dat',
   headerTitle: "dat's Blog",
   description: 'My desire to practice my skills and share my acquired knowledge fuels my endeavors.',
-  theme: 'system',
-  avatar: '/avatar.svg',
-  email: 'luutiendat03112003.dev@gmail.com',
-  github: 'https://github.com/devluutiendat',
-  Facebook:"http://facebook.com/luu.tien.at.374555",
-  phone: '325933782',
-  socialAccounts: {
-    twitter : 'dat',
-    github: 'dat',
-    linkedin: 'dat',
-    facebook: 'dat.dev',
-  },
-  socialBanner : '/avatar.svg',
+  siteUrl: 'https://portfolio-datluu.vercel.app',
+  socialBanner: '/avatar.svg',
   locale: 'en-US',
-  keywords:[
+  keywords: [
     'Web Developer',
     'Portfolio',
     'Next.js',
@@ -50,12 +15,9 @@
     'Frontend Developer',
     'Vietnam Developer'
   ],
-  search: {
-    provider: 'kbar',
-    kbarConfig: {
-      searchDocumentsPath: `${process.env.BASE_PATH || ''}/search.json`,
-    },
-  },
+  socialAccounts: {
+    twitter: 'dat'
+  }
 };
 
 export default siteMetadata;
