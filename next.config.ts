df5d991adac099e8a343f00e21f111a15033c9fb
dat <goodboy30112003@gmail.com>
2025-06-30 17:20:55 +0700

    analyze package

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+const nextConfig: NextConfig = withBundleAnalyzer({
   images: {
     remotePatterns: [
       {
@@ -8,8 +11,10 @@ const nextConfig: NextConfig = {
         hostname: "media.giphy.com",
       }
     ]
+    ,
+    
   }
   
-};
-
+});
 export default nextConfig;
+
